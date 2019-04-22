25d52222f5c1f98bc020f16ad39285c08bffaa16
Elena Perers <elena.perers@chasacademy.se>
2019-04-22 17:21:52 +0200

    Changed code for the class Dice.

--- src/Dice/Dice.php
+++ src/Dice/Dice.php
@@ -1,30 +1,23 @@
 <?php
 /**
- * Guessing game.
+ * Dice game.
  */
 
 namespace Elpr\Dice;
 
 /**
- * Guess my number, a class supporting the game through GET, POST and SESSION.
+ * Showing off a standard class with methods and properties.
  */
 class Dice
 {
     /**
-     * @var int $number   The current secret number.
-     * @var int $tries    Number of tries a guess has been made.
-     * @var int $guess    Players guess.
-     * @var string $doInit    Starts game.
-     * @var string $doGuess    Make a guess.
-     * @var string $doCheat    Show secret number.
+     * @var string  $number   The name of the person.
+     * @var integer $tries    The tries of the person.
      */
-    public $number;
+    public $numbers;
     public $tries;
-    public $guess;
-    public $doInit;
-    public $doGuess;
-    public $doCheat;
-
+    public $sides;
+    private $lastRoll;
 
 
     /**
@@ -37,136 +30,41 @@ class Dice
      *                    default 6.
      */
 
-    public function __construct(int $number = -1, int $tries = 6)
+    public function __construct(int $sides = 6, int $tries = 1)
     {
-        $this->number = $number;
+        $this->numbers = array();
         $this->tries = $tries;
-        $this->random();
+        $this->sides = $sides;
     }
 
     /**
-     * Set the string to start game.
+     * Returns the lastRoll.
      *
-     * @param string $doInit Starts game.
-     *
-     * @return void
+     * @return int
      */
-    public function setInit(string $doInit)
-    {
-        $this->doInit = $doInit;
-    }
 
-    /**
-     * Set the string to make guess.
-     *
-     * @param string $doGuess Make a guess.
-     *
-     * @return void
-     */
-    public function setGuess(string $doGuess)
+    public function getLastRoll()
     {
-        $this->doGuess = $doGuess;
-    }
-
-
-    /**
-     * Set the string to show secret number.
-     *
-     * @param string $doCheat Show secret number.
-     *
-     * @return void
-     */
-    public function setCheat(string $doCheat)
-    {
-        $this->doCheat = $doCheat;
+        return $this->lastRoll;
     }
 
     /**
      * Randomize the secret number between 1 and 100 to initiate a new game.
      *
-     * @return void
+     * @return int
      */
 
-    private function random()
+    public function roll()
     {
-        $this->number = rand(1, 100);
+        $this->lastRoll = rand(1, $this->sides);
+        return $this->lastRoll;
     }
 
-
-    /**
-     * Reset number of tries.
-     *
-     * @return void
-     */
-
-    public function resetTries()
+    public function dice()
     {
-        $this->tries = 6;
-    }
-
-
-
-    /**
-     * Get number of tries left.
-     *
-     * @return int as number of tries made.
-     */
-
-    public function tries()
-    {
-        return $this->tries;
-    }
-
-
-
-    /**
-     * Get the secret number.
-     *
-     * @return int as the secret number.
-     */
-
-    public function number()
-    {
-        return $this->number;
-    }
-
-
-
-
-    /**
-     * Make a guess, decrease remaining guesses and return a string stating
-     * if the guess was correct, too low or to high or if no guesses remains.
-     *
-     * @throws GuessException when guessed number is out of bounds.
-     *
-     * @return string to show the status of the guess made.
-     *
-     * @param int $guess namber that has been guessed.
-     */
-
-    public function makeGuess(int $guess)
-    {
-        $res = "";
-        if ($guess > 100 || $guess < 1) {
-            throw new GuessException("Guess is not between 1 and 100.");
-        } else {
-            if ($this->doInit === "Start from beginning" || $this->number === -1) {
-                //$res = $this->number;
-                // Init the game.
-                $this->random();
-                $this->resetTries();
-            } elseif ($this->doGuess) {
-                // Do a guess.
-                $this->tries -= 1;
-                if ($guess === $this->number) {
-                    $res = "CORRECT";
-                } elseif ($guess > $this->number) {
-                    $res = "TOO HIGH";
-                } else {
-                    $res = "TOO LOW";
-                }
-            }
+        while ($this->tries <= 6) {
+            $this->numbers[$this->tries] = $this->roll();
+            $this->tries ++;
         }
-        return $res;
     }
 }
