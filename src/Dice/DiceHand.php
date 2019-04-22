3900a734f2d8ded6fdb34b3ad3f443f8a0712098
Elena Perers <elena.perers@chasacademy.se>
2019-04-22 17:24:56 +0200

    Added DiceHand class

---
+++ src/Dice/DiceHand.php
@@ -0,0 +1,74 @@
+<?php namespace Elpr\Dice;
+
+/**
+ * A dicehand, consisting of dices.
+ */
+class DiceHand
+{
+    /**
+     * @var Dice $dices   Array consisting of dices.
+     * @var int  $values  Array consisting of last roll of the dices.
+     */
+    private $dices;
+    private $values;
+
+    /**
+     * Constructor to initiate the dicehand with a number of dices.
+     *
+     * @param int $dices Number of dices to create, defaults to five.
+     */
+    public function __construct(int $dices = 5)
+    {
+        $this->dices  = [];
+        $this->values = [];
+
+        for ($i = 0; $i < $dices; $i++) {
+            $this->dices[]  = new Dice();
+            $this->values[] = null;
+        }
+    }
+
+    /**
+     * Roll all dices save their value.
+     *
+     * @return void.
+     */
+    public function roll()
+    {
+        $counter = 0;
+        foreach ($this->dices as $dice) {
+            $this->values[$counter] = $dice->roll();
+            $counter ++;
+        }
+    }
+
+    /**
+     * Get values of dices from last roll.
+     *
+     * @return array with values of the last roll.
+     */
+    public function values()
+    {
+        return $this->values;
+    }
+
+    /**
+     * Get the sum of all dices.
+     *
+     * @return int as the sum of all dices.
+     */
+    public function sum()
+    {
+        return array_sum($this->values);
+    }
+
+    /**
+     * Get the average of all dices.
+     *
+     * @return float as the average of all dices.
+     */
+    public function average()
+    {
+        return array_sum($this->values) / count($this->values);
+    }
+}
