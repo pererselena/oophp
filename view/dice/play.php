92d28518ecb06994a538ef5f1f124a166ba789f5
Elena Perers <elena.perers@chasacademy.se>
2019-04-23 19:45:08 +0200

    Change view to use dice hand to play the game

--- view/dice/play.php
+++ view/dice/play.php
@@ -8,26 +8,31 @@ namespace Anax\View;
 
 // Show incoming variables and view helper functions
 //echo showEnvironment(get_defined_vars(), get_defined_functions());
-$dice = new \Elpr\Dice\DiceGraphic();
-$rolls = 6;
-$res = [];
-$class = [];
-for ($i = 0; $i < $rolls; $i++) {
-    $res[] = $dice->roll();
-    $class[] = $dice->graphic();
-}
+$dice = new \Elpr\Dice\DiceHand();
+$dice->roll();
+$sum = $dice->sum();
+// $res = [];
+// $class = [];
+// for ($i = 0; $i < $rolls; $i++) {
+//     $res[] = $dice->roll();
+//     $class[] = $dice->graphic();
+// }
 
 ?><main>
-    <h1><?= $current->name ?> rolling <?= $rolls ?> graphic dices</h1>
+    <h1><?= $current->name ?> rolling dices</h1>
 
     <p>
-    <?php foreach ($class as $value) : ?>
+    <?php foreach ($dice->getClassNames() as $value) : ?>
         <i class="dice-sprite <?= $value ?>"></i>
     <?php endforeach; ?>
     </p>
 
-    <p><?= implode(", ", $res) ?></p>
-    <p>Sum is: <?= array_sum($res) ?>.</p>
-    <p>Average is: <?= round(array_sum($res) / 6, 1) ?>.</p>
+    <p><?= implode(", ", $dice->values()) ?></p>
+    <?php if ($sum == -1) : ?>
+        <p>You lost all the score!!!</p>
+    <?php else : ?>
+        <p>Sum is: <?= $sum ?>.</p>
+    <?php endif; ?>
+
 
 </main>
