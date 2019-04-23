ebb3536004766455128105fed7a503256dd8088e
Elena Perers <elena.perers@chasacademy.se>
2019-04-23 18:10:46 +0200

    Added a player class

---
+++ src/Dice/Player.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Dice game.
+ */
+
+namespace Elpr\Dice;
+
+/**
+ * Showing off a standard class with methods and properties.
+ */
+class Player
+{
+    /**
+     * @var string  $name   The name of the person.
+     * @var integer $currentScore    Current score of the raund.
+     * @var integer $totalScore    Total score of the game.
+     */
+    public $name;
+    public $currentScore;
+    public $totalScore;
+
+
+
+    /**
+     * Constructor to initiate the object with the players name,
+     *
+     * @param int $name The players name.
+     */
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+        $this->currentScore = 0;
+        $this->totalScore = 0;
+    }
+
+}
