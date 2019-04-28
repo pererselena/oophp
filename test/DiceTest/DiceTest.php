711077ea2b0f1e2586fb75af4409405f9b705e4b
Elena Perers <elena.perers@chasacademy.se>
2019-04-28 13:52:26 +0200

    Created test cases for the dice class

---
+++ test/DiceTest/DiceTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Elpr\Dice;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test for the dice class.
+ */
+class DiceTest extends TestCase
+{
+    /**
+     * Construct object and verify that the object has the expected
+     * properties. Use no arguments.
+     */
+    public function testCreateObjectNoArguments()
+    {
+        $dice = new Dice();
+        $this->assertInstanceOf("\Elpr\Dice\Dice", $dice);
+
+        $res = $dice->tries;
+        $exp = 1;
+        $this->assertEquals($exp, $res);
+
+        $res = $dice->sides;
+        $exp = 6;
+        $this->assertEquals($exp, $res);
+    }
+
+
+
+    /**
+     * Construct object and verify that the object has the expected
+     * properties. Use only first argument.
+     */
+    public function testCreateObjectFirstArgument()
+    {
+        $dice = new Dice(5);
+        $this->assertInstanceOf("\Elpr\Dice\Dice", $dice);
+
+        $res = $dice->tries;
+        $exp = 1;
+        $this->assertEquals($exp, $res);
+
+        $res = $dice->sides;
+        $exp = 5;
+        $this->assertEquals($exp, $res);
+    }
+
+
+
+    /**
+     * Construct object and verify that the object has the expected
+     * properties. Use both arguments.
+     */
+    public function testCreateObjectBothArguments()
+    {
+        $dice = new Dice(4, 2);
+        $this->assertInstanceOf("\Elpr\Dice\Dice", $dice);
+
+        $res = $dice->tries;
+        $exp = 2;
+        $this->assertEquals($exp, $res);
+
+        $res = $dice->sides;
+        $exp = 4;
+        $this->assertEquals($exp, $res);
+    }
+
+    /**
+     * Testing getLastRoll method.
+     */
+    public function testGetLastRoll()
+    {
+        $dice = new Dice(1);
+        $this->assertInstanceOf("\Elpr\Dice\Dice", $dice);
+
+        $dice->roll();
+        $res = $dice->getLastRoll();
+        $exp = 1;
+        $this->assertEquals($exp, $res);
+
+        $dice2 = new Dice(2);
+        $dice2->roll();
+        $res = $dice2->getLastRoll();
+        $min = 0;
+        $max = 3;
+        $this->assertThat(
+            $res,
+            $this->logicalAnd(
+                $this->greaterThan($min),
+                $this->lessThan($max)
+            )
+        );
+    }
+
+    /**
+     * Testing roll method.
+     */
+    public function testRoll()
+    {
+        $dice = new Dice(1);
+        $this->assertInstanceOf("\Elpr\Dice\Dice", $dice);
+
+        $res = $dice->roll();
+        $exp = 1;
+        $this->assertEquals($exp, $res);
+
+        $dice2 = new Dice(2);
+        $res = $dice2->sides;
+        $min = 0;
+        $max = 3;
+        $this->assertThat(
+            $res,
+            $this->logicalAnd(
+                $this->greaterThan($min),
+                $this->lessThan($max)
+            )
+        );
+    }
+}
