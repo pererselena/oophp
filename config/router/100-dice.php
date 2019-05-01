c5b114a05d3f6991281ebc000c830f113030ee4f
Elena Perers <elena.perers@chasacademy.se>
2019-05-01 16:48:09 +0200

    Created configuration file for the dice game

---
+++ config/router/100-dice.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Dice controller.
+ */
+return [
+    // All routes in order
+    "routes" => [
+        [
+            "info" => "Dice controller.",
+            "mount" => "dice1",
+            "handler" => "\Elpr\Dice\DiceController",
+        ],
+    ]
+];
