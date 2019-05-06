51a8f65a0ad2e26137f3e7edaa9017a1fcf195f0
Elena Perers <elena.perers@chasacademy.se>
2019-05-06 21:53:36 +0200

    Created database setup file

--- htdocs/php-pdo-mysql/sql/setup.sql
+++ htdocs/php-pdo-mysql/sql/setup.sql
@@ -6,9 +6,15 @@
 --
 -- Create the database with a testuser
 --
--- CREATE DATABASE IF NOT EXISTS oophp;
--- GRANT ALL ON oophp.* TO user@localhost IDENTIFIED BY "pass";
--- USE oophp;
+CREATE DATABASE IF NOT EXISTS elpr18;
+CREATE USER IF NOT EXISTS 'elpr18'@'%'
+    IDENTIFIED WITH mysql_native_password BY 'NJUJcEKR2Vq6'
+;
+
+GRANT ALL PRIVILEGES
+	ON elpr18.* 
+	TO 'elpr18'@'%';
+USE elpr18;
 
 -- Ensure UTF8 as chacrter encoding within connection.
 SET NAMES utf8;
@@ -17,8 +23,8 @@ SET NAMES utf8;
 --
 -- Create table for my own movie database
 --
-DROP TABLE IF EXISTS `movie`;
-CREATE TABLE `movie`
+DROP TABLE IF EXISTS `kmom05_movie`;
+CREATE TABLE `kmom05_movie`
 (
     `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     `title` VARCHAR(100) NOT NULL,
@@ -33,8 +39,8 @@ CREATE TABLE `movie`
     `format` CHAR(3) DEFAULT NULL         -- mp4, divx, etc
 ) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
 
-DELETE FROM `movie`;
-INSERT INTO `movie` (`title`, `year`, `image`) VALUES
+DELETE FROM `kmom05_movie`;
+INSERT INTO `kmom05_movie` (`title`, `year`, `image`) VALUES
     ('Pulp fiction', 1994, 'img/pulp-fiction.jpg'),
     ('American Pie', 1999, 'img/american-pie.jpg'),
     ('Pokémon The Movie 2000', 1999, 'img/pokemon.jpg'),  
@@ -42,4 +48,4 @@ INSERT INTO `movie` (`title`, `year`, `image`) VALUES
     ('From Dusk Till Dawn', 1996, 'img/from-dusk-till-dawn.jpg')
 ;
 
-SELECT * FROM `movie`;
+SELECT * FROM `kmom05_movie`;
