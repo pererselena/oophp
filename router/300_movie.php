22479675ccadfe58a5d5d9271afaad68f4205a74
Elena Perers <elena.perers@chasacademy.se>
2019-05-07 12:23:54 +0200

    Created route for movie

---
+++ router/300_movie.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Create routes using $app programming style.
+ */
+//var_dump(array_keys(get_defined_vars()));
+
+
+
+/**
+ * Show all movies.
+ */
+$app->router->get("movie", function () use ($app) {
+    $title = "Movie database | oophp";
+
+    $app->db->connect();
+    $sql = "SELECT * FROM movie;";
+    $res = $app->db->executeFetchAll($sql);
+
+    $app->page->add("movie/index", [
+        "res" => $res,
+    ]);
+
+    return $app->page->render([
+        "title" => $title,
+    ]);
+});
