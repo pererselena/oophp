409fd9165399b8832c65cf9eef038ec38f2a0cc8
Elena Perers <elena.perers@chasacademy.se>
2019-05-09 18:28:18 +0200

    Added view for delete

---
+++ view/movie/delete.php
@@ -0,0 +1,25 @@
+<?php namespace Anax\View;
+?>
+
+<form method="post">
+    <fieldset>
+    <legend>Edit</legend>
+    <label>Id:</label><br>
+    <input disabled name="id" value="<?= $resultset->id ?>"/>
+    <br>
+    <label>Title:</label><br>
+    <input disabled type="text" name="title" value="<?= $resultset->title ?>"/>
+    <br>
+    <label>Year:</label><br>
+    <input disabled type="number" name="year" value="<?= $resultset->year ?>"/>
+    <br>
+    <label>Image:</label><br>
+    <input disabled type="text" name="image" value="<?= $resultset->image ?>"/>
+    <br>
+    <input type="submit" name="doDelete" value="Delete"><br>
+
+    <p>
+        <a href="../movie">Show all</a>
+    </p>
+    </fieldset>
+</form>
