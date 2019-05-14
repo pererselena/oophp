b4012c3480fe7c50b0a7a4b1038f40de1f876c4a
Elena Perers <elena.perers@chasacademy.se>
2019-05-14 23:14:04 +0200

    Mounted textfilter controller

---
+++ config/router/300-textfilter.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * TextFilter controller.
+ */
+return [
+    // All routes in order
+    "routes" => [
+        [
+            "info" => "TextFilter controller.",
+            "mount" => "textfilter",
+            "handler" => "\Elpr\MyTextFilter\MyTextFilterController",
+        ],
+    ]
+];
